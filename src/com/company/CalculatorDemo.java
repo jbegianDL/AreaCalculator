a46f913e6f2fb40b909ea1d1329689a39abac392
Jordan Begian <jordanbegian@Jordans-MacBook-Pro.local>
2019-01-22 15:17:23 -0500

    removed instruction comments

--- src/com/company/CalculatorDemo.java
+++ src/com/company/CalculatorDemo.java
@@ -1,23 +1,3 @@
-//Create a new project in IntelliJ named AreaCalculator using the Command Line App template
-//
-//Initialize your project as a Git repository and be sure to add and commit all changes as you work
-//
-//Rename your Main file to CalculatorDemo (right-click -> Refactor -> Rename...)
-//
-//Create a new Java class file (File -> New -> Java Class) named Calculator
-//
-//Your Calculator should have (2) properties: width and height (with accessor methods)
-//
-//Your Calculator should have a method named calculateArea() which:
-//
-//Accepts width and height as parameters
-//
-//Calculates the area of the rectangle
-//
-//Returns the area
-//
-//In your CalculatorDemo, create an instance of Calculator and test your calculateArea() method
-
 package com.company;
 
 public class CalculatorDemo {
@@ -26,7 +6,7 @@ public class CalculatorDemo {
 
         Calculator myCalc = new Calculator();
 
-        System.out.println(myCalc.calculateArea(3,5));
+        System.out.println(myCalc.calculateArea(4,5));
 
     }
 }
